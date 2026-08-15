b36ecb0573030f66732d23f4f82f2e3d2cb869ec
Lucky-chikezie <ugwuchikezielucky@gmail.com>
2026-08-15 16:28:17 +0100

    rearranged generateToken for readability.

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 function generateToken(userId) {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
-  });
+  const token=jwt.sign(
+    {id: userId},
+    process.env.JWT_SECRET,
+    {expiresIn: process.env.JWT_EXPIRES_IN || '7d'}
+  );
+  return token;
 }
 
 module.exports = generateToken;
