4b83330bca1076bc974edc4f05fb3573da4f09b2
Lucky-chikezie <ugwuchikezielucky@gmail.com>
2026-08-15 16:41:24 +0100

    refactor:: simplified database connection loggin

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
 async function connectDB() {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error(`Failed to connect to MongoDB: ${err.message}`);
+  try{
+    const connection = await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected: ' + connection.connection.host);
+  }
+  catch(error) {
+    console.log('Failed to connect to MongoDB');
+    console.log(error.message);
     process.exit(1);
   }
 }
